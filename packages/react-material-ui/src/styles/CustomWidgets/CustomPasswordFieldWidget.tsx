359c54e499f6cb4bc2e4c33dcc3eff545cc0a22b
Rodrigo Dias <rodrigo.elfo@gmail.com>
2022-12-02 10:48:31 -0300

    feat: build 6

--- packages/react-material-ui/src/styles/CustomWidgets/CustomPasswordFieldWidget.tsx
+++
@@ -1,9 +0,0 @@
-import React, { FC } from 'react';
-import { WidgetProps } from '@rjsf/utils';
-import CustomTextFieldWidget from './CustomTextFieldWidget';
-
-const CustomPasswordFieldWidget: FC<WidgetProps> = (props) => (
-  <CustomTextFieldWidget {...props} type="password" />
-);
-
-export default CustomPasswordFieldWidget;
